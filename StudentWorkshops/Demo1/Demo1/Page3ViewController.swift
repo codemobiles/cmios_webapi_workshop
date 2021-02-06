b70a1e02f9a8fef047853ef3ae2be106208fbebd
codemobiles <chaiyasit.t@gmail.com>
2021-02-06 15:48:38 +0700

    1234

--- StudentWorkshops/Demo1/Demo1/Page3ViewController.swift
+++ StudentWorkshops/Demo1/Demo1/Page3ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
-
+import Alamofire
 class Page3ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.uploadFile(url: targetURL, data: <#T##Data#>, fileName: <#T##String#>)
         // Do any additional setup after loading the view.
     }
+    
+    
+    func uploadFile(url: String, data: Data, fileName: String) {
+        let parameter = ["username": "ABCD", "password": "12345678"]
+        
+        AF.upload(multipartFormData: { MultipartFormData in
+            MultipartFormData.append(data, withName: "image", fileName: fileName, mimeType: "image/jpg")
+            for(key,value) in parameter{
+                MultipartFormData.append(value.data(using: .utf8)!, withName: key)
+            }
+        }, to: url, method: .post).uploadProgress(queue: .main, closure: { progress in
+            //Current upload progress of file
+            //print("Upload Progress: \(progress.fractionCompleted)")
+        }).responseString(completionHandler: { (response) in
+            switch response.result {
+            case .success(let value):
+                print(value)
+                break
+            case .failure:
+                print("failure")
+                break
+            }
+        })
+    }
+    
     
 
     /*
