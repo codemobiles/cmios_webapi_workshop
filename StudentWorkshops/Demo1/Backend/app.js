c2e3fd2a76752995d46e176624dbe56a0195a5c2
codemobiles <chaiyasit.t@gmail.com>
2021-02-06 14:13:34 +0700

    1234

--- StudentWorkshops/Demo1/Backend/app.js
+++ StudentWorkshops/Demo1/Backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+
+app.use(bodyParser.json())
+
 app.post("/login", (req, res)=>{
-    res.json({result: 'ok'})
+    res.json({result: 'ok', echo: req.body})
 })
 
 app.listen(8080, ()=>{
