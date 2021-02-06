bdf262ddc37d641d7d8bcff96bd60ec7be9dd4aa
codemobiles <chaiyasit.t@gmail.com>
2021-02-06 13:17:18 +0700

    1234

---
+++ StudentWorkshops/Demo1/Demo1/Models/YoutubePlaylist.swift
@@ -0,0 +1,31 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let youtubePlaylist = try? newJSONDecoder().decode(YoutubePlaylist.self, from: jsonData)
+
+import Foundation
+
+// MARK: - YoutubePlaylist
+struct YoutubePlaylist: Codable {
+    let youtubes: [Youtube]?
+    let error: Bool?
+    let errorMsg: String?
+
+    enum CodingKeys: String, CodingKey {
+        case youtubes, error
+        case errorMsg
+    }
+}
+
+// MARK: - Youtube
+struct Youtube: Codable {
+    let id, title, subtitle: String?
+    let avatarImage: String?
+    let youtubeImage: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id, title, subtitle
+        case avatarImage
+        case youtubeImage
+    }
+}
