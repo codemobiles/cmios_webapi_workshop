da6f54f83ba838694abf0e779dc7ef086a34e99e
codemobiles <chaiyasit.t@gmail.com>
2021-02-06 08:52:27 +0700

    1234

---
+++ CM_IOSWebAPIDemo/CM_IOSWebAPIDemo/ViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ViewController.swift
+//  CM_IOSWebAPIDemo
+//
+//  Created by chaiyasit on 2/5/21.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.testLink1()
+//        self.testLink2()
+//        self.testLink3()
+//        self.testLink4()
+//        self.testLink5()
+        self.testLink6()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func testLink1()  {
+        AF.request("https://google.com").responseString { (response) in
+            switch response.result {
+            case .success(let result):
+                print(result)
+                break
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
+    
+    
+    func testLink2()  {
+        AF.request("http://jsonplaceholder.typicode.com/posts").responseString { (response) in
+            switch response.result {
+            case .success(let result):
+                print(result)
+                break
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
+    
+    func testLink3()  {
+        AF.request("http://jsonplaceholder.typicode.com/posts", method: .get).responseString { (response) in
+            switch response.result {
+            case .success(let result):
+                print(result)
+                break
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
+    
+    func testLink4()  {
+        let data:[String:String] = ["title":"codemobiles", "body":"iOS", "userId": "6"]
+        AF.request("http://jsonplaceholder.typicode.com/posts", method: .post, parameters: data).responseString { (response) in
+            switch response.result {
+            case .success(let result):
+                print(result)
+                break
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
+    
+    
+    func testLink5()  {
+        let data:[String:String] = ["title":"codemobiles", "body":"iOS", "userId": "6"]
+        AF.request("http://jsonplaceholder.typicode.com/posts", method: .post, parameters: data).responseJSON { (response) in
+            switch response.result {
+            case .success(_):
+                guard let data = response.data else {return}
+                do {
+                    let decoder = JSONDecoder()
+                    let product =  try decoder.decode(Product.self, from: data)
+                    print("\(Product.CodingKeys.id.stringValue) : \(product.id!)")
+                    print("\(Product.CodingKeys.userID.stringValue) : \(product.userID!)")
+                    print("\(Product.CodingKeys.title.stringValue) : \(product.title!)")
+                    print("\(Product.CodingKeys.body.stringValue) : \(product.body!)")
+                }catch let error {
+                    print(error)
+                }
+                break
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
+        
+    func testLink6()  {
+        AF.request("http://jsonplaceholder.typicode.com/posts", method: .get).responseJSON { (response) in
+            switch response.result {
+            case .success(_):
+                guard let data = response.data else {return}
+                do {
+                    let decoder = JSONDecoder()
+                    let products =  try decoder.decode([Product].self, from: data)
+                    for product in products {
+                        print("\(Product.CodingKeys.id.stringValue) : \(product.id!)")
+                        print("\(Product.CodingKeys.userID.stringValue) : \(product.userID!)")
+                        print("\(Product.CodingKeys.title.stringValue) : \(product.title!)")
+                        print("\(Product.CodingKeys.body.stringValue) : \(product.body!)")
+                        print("\n\n")
+                    }
+                    
+                }catch let error {
+                    print(error)
+                }
+                break
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
+}
+
+struct Product: Codable {
+    let body, title: String?
+    let id, userID: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case body, title
+        case userID = "userId"
+        case id
+    }
+}
--- readme.txt
+++ readme.txt
@@ -1,1 +1,18 @@
 Course Outline
+1. Setp Xcode, VScode, RESTFul Client, NodeJS 14.x
+2. New Project with Cocoa
+3. Intro Http Network Programming
+   - Http Protocol : [GET, POST, DELETE, PUT..]
+   - HTTP GET vs POST   
+   - What is RESTfulAPI?
+   - URL, StringQuery, Parameter, HTTP Body, RAWJSON, Header
+4. Test Http Target with POSTMan, VSCode RESTful, Curl
+5. Intro Alamofire
+6. JSONPlaceHolder https://jsonplaceholder.typicode.com/
+
+
+Workshop
+1. HTTPS: AF.request("https://google.com").responseString { (response) in
+2. HTTP: AF.request("http://jsonplaceholder.typicode.com/posts").responseString { (response) in
+3. HTTP GET Method: AF.request("http://jsonplaceholder.typicode.com/posts", method: .get).responseString { (response) in
+4. HTTP POST Method: AF.request("http://jsonplaceholder.typicode.com/posts", method: .post, parameters: data).responseString { (response) in
