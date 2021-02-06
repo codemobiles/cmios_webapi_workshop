455cc55a2d7a1f43eb2e984ea6ed9fed9228fcbb
codemobiles <chaiyasit.t@gmail.com>
2021-02-06 14:04:58 +0700

    1234

--- StudentWorkshops/Demo1/Backend/app.js
+++ StudentWorkshops/Demo1/Backend/app.js
@@ -1,1 +1,10 @@
-console.log("CodeMobiles")
+const express = require('express')
+const app = express()
+
+app.post("/login", (req, res)=>{
+    res.json({result: 'ok'})
+})
+
+app.listen(8080, ()=>{
+    console.log("Ready")
+})
