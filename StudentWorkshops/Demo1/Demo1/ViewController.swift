66003162f77cf24c49e8d0f28925e7823db1c028
codemobiles <chaiyasit.t@gmail.com>
2021-02-06 09:56:28 +0700

    1234

--- StudentWorkshops/Demo1/Demo1/ViewController.swift
+++ StudentWorkshops/Demo1/Demo1/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
     // Lek - CodeMobiles
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.testLink1()
         // Do any additional setup after loading the view.
     }
 
 
+    func testLink1(){
+        AF.request("https://google.com").responseString { (response) in
+            switch response.result{
+            case .success(let result):
+               print(result)
+            break
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
 }
 
